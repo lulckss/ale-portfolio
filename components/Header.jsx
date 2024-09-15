6f8a007c7d1ba4a398b89a97acc5c83edc862f0f
Alessandro Lucas dos Santos de Oliveira <106298141+lulckss@users.noreply.github.com>
2024-09-15 20:27:25 -0300

    Initial commit

---
+++ components/Header.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+// componentes
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* logo */}
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Ale<span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        {/* desktop nav e botao contrate-me */}
+        <div className="hidden xl:flex items-center gap-8">
+         <Nav />
+         <Link href="/contact">
+         <Button>Contrate-me</Button>
+         </Link>  
+        </div>
+
+
+        {/*mobile nav */}
+        <div className="xl:hidden">mobile nav</div>
+      </div>
+    </header>
+  );
+};
+        
+export default Header;
+
